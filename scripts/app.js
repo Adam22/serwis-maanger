fc796d101ddca5411021855f2a113f6b7f936db5
Adam22 <ad.tumi@gmail.com>
2016-04-07 22:26:39 +0200

    right navigation, templates + right navigation

--- scripts/app.js
+++ scripts/app.js
@@ -10,22 +10,30 @@ var app = angular
   .constant('FURL', 'https://serwis-manager.firebaseio.com/')    
   .config(function ($routeProvider) {
     $routeProvider      
-        .when('/', {
-            templateUrl: 'views/main.html'        
-        })
-        .when('/browse', {
-            templateUrl: 'views/browse.html',
-            controller: 'ClientController'
-        })
-        .when('/client', {
-            templateUrl: 'views/client.html',
-            controller: 'ClientController'
-        })
-        .when('/edit/:clientId', {
-            templateUrl: 'views/edit.html',
-            controller: 'ClientController'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+      .when('/', {
+        templateUrl: 'views/main.html'        
+      })
+    .when('/login', {
+        templateUrl: 'views/login.html'
+        
+      })
+    .when('/register', {
+        templateUrl: 'views/register.html'
+        
+      })
+    .when('/browse', {
+        templateUrl: 'views/browse.html',
+        controller: 'ClientController'
+      })
+    .when('/client', {
+        templateUrl: 'views/client.html',
+        controller: 'ClientController'
+      })
+    .when('/edit/:clientId', {
+        templateUrl: 'views/edit.html',
+        controller: 'ClientController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
   });
